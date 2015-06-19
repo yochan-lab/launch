f5748a9ad722756a7af1f7f442c1dfde24699044
Jake Merdich <jake@merdich.com>
2015-06-19 16:25:55 -0700

    Scripting magic.

---
+++ env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+export BOT_HOSTNAME=""
+export LOC_HOSTNAME="localhost"
+
+export ROS_HOME_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export ROS_HOSTNAME="`hostname`.local"
+export ROS_MASTER_URI="http://$LOC_HOSTNAME/"
+
+alias rec_pull="cd $ROS_HOME_DIR; git submodule init && git pull --recurse-submodules && git submodule status"
+alias rec_init="cd $ROS_HOME_DIR; rec_pull && catkin init"
+alias goloc="export ROS_MASTER_URI=\"http://$LOC_HOSTNAME:11311/\""
+alias gobot="export ROS_MASTER_URI=\"http://$BOT_HOSTNAME:11311/\""
+# if usr not in path....
+if [[ ":$PATH:" == *":/usr/local:"* ]]; then
+  export CMAKE_PREFIX_PATH=/usr/local:$CMAKE_PREFIX_PATH
+fi
